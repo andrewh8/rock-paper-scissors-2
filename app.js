3fc8bab0bcd6de42436a99481c90e8e50538f6a0
andrewh8 <andrewhardesty8@gmail.com>
2023-07-21 21:57:38 -0400

    wrote logic for console-based game

--- app.js
+++ app.js
@@ -1,47 +1,90 @@
-function getComputerChoice() {
-    const number = Math.random()*10;
-    let choice = "";
-
-    if (number< 3.33) {
-        choice = "Rock";
-    } else if (number >= 3.33 && number <= 6.66) {
-        choice = "Scissors";
-    } else {
-        choice = "Paper";
-    }
-
-    return choice;
-}
-
-function getUserChoice() {
-    let choice = prompt("Rock, Paper, Scissors?");
-    
-    while (choice != "Rock" && choice != "Paper" && choice != "Scissors") {
-        choice = prompt("Choose one of the following: Rock, Paper, Scissors?");
-    }
-    return choice;
-}
-
-let userChoice = getComputerChoice();
-let computerChoice = getComputerChoice();
+let userChoice = "";
+let computerChoice = "";
 let result = "";
 let userScore = 0;
 let computerScore = 0;
+let winner = "";
 
-switch(userChoice = "Rock") {
-    case computerChoice = "Rock":
-        result = "Tie";
-        break;
-    case computerChoice = "Scissor":
-        result = "You Win";
-        userScore = userScore + 1;
-        break    
+function getComputerChoice() {
+    const number = Math.random()*10;
+
+    if (number< 3.33) {
+        computerChoice = "Rock";
+    } else if (number >= 3.33 && number <= 6.66) {
+        computerChoice = "Scissors";
+    } else {
+        computerChoice = "Paper";
+    }
+
+    return computerChoice;
 }
 
-// scissors < rock < paper 
-// rock < paper < scissors
-// paper < scissors < rock
+function getUserChoice() {
+    userChoice = prompt("Rock, Paper, Scissors?");
+    
+    while (userChoice != "Rock" && userChoice != "Paper" && userChoice != "Scissors") {
+        userChoice = prompt("Choose one of the following: Rock, Paper, Scissors?");
+    }
+    return userChoice;
+}
 
+function determineRoundWinner() {
+    // User chooses Rock
+    if (userChoice == "Rock") {
+        if (computerChoice == "Rock") {
+            result = "Tie!";
+        } else if (computerChoice == "Scissors") {
+            result = "You Win!";
+            userScore++;
+        } else if (computerChoice == "Paper") {
+            result = "You Lose!";
+            computerScore++;
+        }
+    } 
+    // User chooses Paper
+        else if (userChoice == "Paper") {
+            if (computerChoice == "Paper") {
+                result = "Tie!";
+            } else if (computerChoice == "Rock") {
+                result = "You Win!";
+                userScore++;
+            } else if (computerChoice == "Scissors") {
+                result = "You Lose!";
+                computerScore++;
+            }
+        }
+    // User chooses Scissors
+        else if (userChoice == "Scissors") {
+            if (computerChoice == "Scissors") {
+                result = "Tie!";
+            } else if (computerChoice == "Paper") {
+                result = "You Win!";
+                userScore++;
+            } else if (computerChoice == "Rock") {
+                result = "You Lose!";
+                computerScore++;
+            }
+        }
+}
 
-console.log(getComputerChoice());
-console.log(getUserChoice());
+function playRound() {
+    getComputerChoice();
+    getUserChoice();
+    determineRoundWinner();
+    console.log(result);
+    console.log(userScore);
+    console.log(computerScore);
+}
+
+function playGame() {
+    while (userScore < 5 && computerScore < 5) {
+        playRound();
+    }
+
+    if (userScore == 5) {
+        winner = "User";
+    } else if (computerScore == 5) {
+        winner = "Computer";
+    }
+    console.log(`The winner is ${winner}!`);
+}
